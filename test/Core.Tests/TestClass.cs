6855cbfecf5a883dca7918e38783737ceb5f8d85
AliReZa Sabouri <alirezanet@outlook.com>
2020-07-05 23:43:41 +0430

    add clone and cunstructor options

--- test/Core.Tests/TestClass.cs
+++ test/Core.Tests/TestClass.cs
@@ -1,8 +1,25 @@
+using System;
+
 namespace Gridify.Tests {
 
-    public class TestClass {
+    public class TestClass : ICloneable {
+        public TestClass () { }
+        public TestClass (int id, string name, TestClass classProp) {
+            this.Id = id;
+            this.Name = name;
+            this.ChildClass = classProp;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
-        public TestClass ClassProp { get; set; }
+        public TestClass ChildClass { get; set; }
+
+        public object Clone () {
+            return new TestClass () {
+                Id = this.Id,
+                    Name = this.Name,
+                    ChildClass = this.ChildClass != null ? (TestClass) this.ChildClass.Clone () : null
+            };
+        }
     }
 }
