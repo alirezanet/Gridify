b75e648531344a34c1eb03c775769d4279e18964
AliReZa Sabouri <alirezanet@outlook.com>
2020-09-17 17:22:27 +0430

    add overrideIfExists test

--- test/Core.Tests/GridifyMapperShould.cs
+++ test/Core.Tests/GridifyMapperShould.cs
@@ -8,41 +8,54 @@ namespace Gridify.Tests
    public class GridifyMapperShould
    {
       private IGridifyMapper<TestClass> _sut;
-      public GridifyMapperShould () => _sut = new GridifyMapper<TestClass> ();     
+      public GridifyMapperShould() => _sut = new GridifyMapper<TestClass>();
 
       [Fact]
-      public void GenerateMappings ()
+      public void GenerateMappings()
       {
-         _sut.GenerateMappings ();
+         _sut.GenerateMappings();
 
-         Assert.Equal (typeof (TestClass).GetProperties ().Count (), _sut.GetCurrentMaps().Count());
-         Assert.True (_sut.HasMap ("Id"));
+         Assert.Equal(typeof(TestClass).GetProperties().Count(), _sut.GetCurrentMaps().Count());
+         Assert.True(_sut.HasMap("Id"));
       }
 
       [Fact]
-      public void CaseSensitivity ()
+      public void CaseSensitivity()
       {
-         var sut = new GridifyMapper<TestClass> (true);
-         sut.AddMap ("id", q => q.Id );
+         var sut = new GridifyMapper<TestClass>(true);
+         sut.AddMap("id", q => q.Id);
 
-         Assert.True (sut.HasMap ("id"));
-         Assert.False (sut.HasMap ("ID"));
+         Assert.True(sut.HasMap("id"));
+         Assert.False(sut.HasMap("ID"));
       }
 
       [Fact]
-      public void AddMap ()
+      public void AddMap()
       {
-         _sut.AddMap (nameof (TestClass.Name), p => p.Name);
-         Assert.Single (_sut.GetCurrentMaps());
+         _sut.AddMap(nameof(TestClass.Name), p => p.Name);
+         Assert.Single(_sut.GetCurrentMaps());
       }
 
       [Fact]
-      public void RemoveMap ()
+      public void RemoveMap()
       {
-         _sut.AddMap ("name", q => q.Name);
-         _sut.AddMap ("Id", q => q.Id);
-         _sut.RemoveMap (nameof (TestClass.Name));
-         Assert.Single (_sut.GetCurrentMaps());
+         _sut.AddMap("name", q => q.Name);
+         _sut.AddMap("Id", q => q.Id);
+         _sut.RemoveMap(nameof(TestClass.Name));
+         Assert.Single(_sut.GetCurrentMaps());
+      }
+
+      [Fact]
+      public void AddMap_DuplicateKey_ShouldThrowErrorIfOverrideIfExistsIsFalse()
+      {
+         //arrange
+         _sut.AddMap("Test", q => q.Name);
+         //act
+         Action act = () => _sut.AddMap("test", q => q.Name, overrideIfExists: false);
+         //assert
+         Exception exception = Assert.Throws<Exception>(act);
+         //The thrown exception can be used for even more detailed assertions.
+         Assert.Equal("Duplicate Key. the 'test' key already exists", exception.Message);
       }
 
    }
