a76d5a52349a0812d59e1f5c42e5a888e86b7d88
Danial Shokouhmanesh <66676348+DeV1no@users.noreply.github.com>
2021-05-18 17:04:28 +0430

    vue files fixed  (#3)
    * fixed vue sample dir
    
    * none framework base added
    
    * character filter Added
    
    * numeric filter added
    
    * complex Filter Added
    
    * custom maping added
    
    * order by added
    
    * sort by page added
    
    * comments added
    
    * fix src files
    
    Co-authored-by: Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
    Co-authored-by: Aria <fresh1995boy@Gmail.com>

---
+++ SampleProject/Client/NoneFrameWork Sample/JS/NumericFilter.js
@@ -0,0 +1,101 @@
+const ageLessThanInput = document.getElementById('ageLessThan');
+const ageMoreThanInput = document.getElementById('ageMoreThan');
+const searchBtn = document.getElementById('searchBtn');
+const clearBtn = document.getElementById('clearBtn');
+const tableBody = document.getElementById('tableBody');
+const proxy = "http://localhost:5000"
+let queryParam = document.getElementById('queryParam')
+let query;
+
+// Fetiching No search Data
+query = proxy + '/api/Gridify'
+fetch(query)
+    .then(response => response.json())
+    .then(data => {
+        data.items.forEach((q, index) => {
+            const headerOne = document.createElement('tr');
+            headerOne.innerHTML = `
+            <th scope="row">${ index+1 } </th>
+            <th scope="row">${ q.firstName } </th>
+            <th scope="row">${ q.lastName } </th>
+            <th scope="row">${ q.age } </th>
+            <th scope="row">${ q.phoneNumber } </th>
+            <th scope="row">${ q.address } </th>
+            `
+            tableBody.append(headerOne)
+        });
+    })
+
+//Search Data with params
+function search() {
+    query = proxy + '/api/Gridify'
+    if (ageLessThanInput.value && !ageMoreThanInput.value) {
+        query = proxy + `/api/Gridify/?Filter=age>>${ageLessThanInput.value}`;
+    }
+    if (!ageLessThanInput.value && ageMoreThanInput.value) {
+        query = proxy + `/api/Gridify/?Filter=age<=${ageMoreThanInput.value}`;
+    }
+    if (ageLessThanInput.value && ageMoreThanInput.value) {
+        query = proxy + `/api/Gridify/?Filter=age>>${ageLessThanInput.value},age<=${ageMoreThanInput.value}`;
+    }
+    queryParam.innerHTML = 'query: ' + query;
+
+    tableBody.innerHTML = ''
+    fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach((q, index) => {
+                const headerOne = document.createElement('tr');
+                headerOne.innerHTML = `
+                <th scope="row">${ index+1 } </th>
+                <th scope="row">${ q.firstName } </th>
+                <th scope="row">${ q.lastName } </th>
+                <th scope="row">${ q.age } </th>
+                <th scope="row">${ q.phoneNumber } </th>
+                <th scope="row">${ q.address } </th>
+                `
+                tableBody.append(headerOne)
+            });
+        })
+}
+
+
+// No Search param
+function noSearchParam() {
+    query = proxy + '/api/Gridify'
+    queryParam.innerHTML = 'query: ' + query;
+    tableBody.innerHTML = ''
+    fetch(query)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach((q, index) => {
+                const headerOne = document.createElement('tr');
+                headerOne.innerHTML = `
+            <th scope="row">${ index+1 } </th>
+            <th scope="row">${ q.firstName } </th>
+            <th scope="row">${ q.lastName } </th>
+            <th scope="row">${ q.age } </th>
+            <th scope="row">${ q.phoneNumber } </th>
+            <th scope="row">${ q.address } </th>
+            `
+                tableBody.append(headerOne)
+
+
+
+            });
+        })
+
+}
+
+// Clear Input values    
+function clear() {
+    ageMoreThanInput.value = '';
+    ageLessThanInput.value = '';
+    noSearchParam();
+}
+queryParam.innerHTML = 'query: ' + query;
+
+
+// Event Listener For btn's
+clearBtn.addEventListener('click', clear)
+searchBtn.addEventListener('click', search)
