ba5ecc51c30db9fe4f7a66d6b8a3a6fdf35ac656
alirezanet <alirezanet@outlook.com>
2021-06-25 21:17:50 +0430

    add constructor to gridifyQuery

--- src/Core/GridifyQuery.cs
+++ src/Core/GridifyQuery.cs
@@ -2,6 +2,15 @@ namespace Gridify
 {
    public class GridifyQuery : IGridifyQuery
    {
+      public GridifyQuery(string sortBy = null, bool isSortAsc = default, short page = default, int pageSize = default, string filter = null)
+      {
+         SortBy = sortBy;
+         IsSortAsc = isSortAsc;
+         Page = page;
+         PageSize = pageSize;
+         Filter = filter;
+      }
+
       public string SortBy { get; set; }
       public bool IsSortAsc { get; set; }
       public short Page { get; set; }
