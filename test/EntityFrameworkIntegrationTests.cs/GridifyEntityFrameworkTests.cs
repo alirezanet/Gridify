64ad0054c96e9b8582c592924005f3bce975159a
alirezanet <alirezanet@outlook.com>
2021-06-25 21:06:54 +0430

    add EF Integration Test Project

---
+++ test/EntityFrameworkIntegrationTests.cs/GridifyEntityFrameworkTests.cs
@@ -0,0 +1,28 @@
+﻿using Gridify;
+using Xunit;
+
+namespace EntityFrameworkIntegrationTests.cs
+{
+   public class GridifyEntityFrameworkTests
+   {
+      private readonly MyDbContext _dbContext;
+
+      public GridifyEntityFrameworkTests()
+      {
+         _dbContext = new MyDbContext();
+      }
+
+      [Fact]
+      public void GridifyQueryableDateTimeShouldNotThrowException()
+      {
+         // arrange
+         var gq = new GridifyQuery {IsSortAsc = true, SortBy = "CreateDate"};
+
+         // act
+         var exception = Record.Exception(() => _dbContext.Users.GridifyQueryable(gq));
+
+         // assert
+         Assert.Null(exception);
+      }
+   }
+}
---
+++ test/EntityFrameworkIntegrationTests.cs/MyDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkIntegrationTests.cs
+{
+   public class MyDbContext : DbContext
+   {
+      public DbSet<User> Users { get; set; }
+
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseInMemoryDatabase("myTestDb");
+         base.OnConfiguring(optionsBuilder);
+      }
+   }
+
+   public class User
+   {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public DateTime CreateDate { get; set; }
+      public Guid FkGuid { get; set; }
+   }
+}
