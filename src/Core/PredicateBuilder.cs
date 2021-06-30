0578e78c1cbad64958a0137b622b1a8743891581
alirezanet <alirezanet@outlook.com>
2021-06-30 22:48:27 +0430

    fix #6 Add Support EF ServiceProviderCaching
    Replace Expression.Invoke with an ExpressionVisitor To support EntityFramework ServiceProviderCaching.

--- src/Core/PredicateBuilder.cs
+++ src/Core/PredicateBuilder.cs
@@ -5,23 +5,52 @@ namespace Gridify
 {
    public static class PredicateBuilder
    {
+      public static Expression<Func<T, bool>> Or<T> (this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+      {
+         var parameter = Expression.Parameter(typeof (T));
+
+         var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+         var left = leftVisitor.Visit(expr1.Body);
+
+         var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+         var right = rightVisitor.Visit(expr2.Body);
+
+         return Expression.Lambda<Func<T, bool>>(Expression.OrElse(left!, right!), parameter);
+      }
+
+      public static Expression<Func<T, bool>> And<T> (this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+      {
+         var parameter = Expression.Parameter(typeof (T) , "q");
+
+         var leftVisitor = new ReplaceExpressionVisitor(expr1.Parameters[0], parameter);
+         var left = leftVisitor.Visit(expr1.Body);
+
+         var rightVisitor = new ReplaceExpressionVisitor(expr2.Parameters[0], parameter);
+         var right = rightVisitor.Visit(expr2.Body);
+
+         return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left!, right!), parameter);
+      }
+      
+      
+      private class ReplaceExpressionVisitor : ExpressionVisitor
+      {
+         private readonly Expression _oldValue;
+         private readonly Expression _newValue;
+
+         public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+         {
+            _oldValue = oldValue;
+            _newValue = newValue;
+         }
+
+         public override Expression Visit(Expression node)
+         {
+            return node == _oldValue ? _newValue : base.Visit(node);
+         }
+      } 
+      
       public static Expression<Func<T, bool>> True<T> () { return f => true; }
       public static Expression<Func<T, bool>> False<T> () { return f => false; }
 
-      public static Expression<Func<T, bool>> Or<T> (this Expression<Func<T, bool>> expr1,
-         Expression<Func<T, bool>> expr2)
-      {
-         var invokedExpr = Expression.Invoke (expr2, expr1.Parameters.Cast<Expression> ());
-         return Expression.Lambda<Func<T, bool>>
-            (Expression.OrElse (expr1.Body, invokedExpr), expr1.Parameters);
-      }
-
-      public static Expression<Func<T, bool>> And<T> (this Expression<Func<T, bool>> expr1,
-         Expression<Func<T, bool>> expr2)
-      {
-         var invokedExpr = Expression.Invoke (expr2, expr1.Parameters.Cast<Expression> ());
-         return Expression.Lambda<Func<T, bool>>
-            (Expression.AndAlso (expr1.Body, invokedExpr), expr1.Parameters);
-      }
    }
 }
