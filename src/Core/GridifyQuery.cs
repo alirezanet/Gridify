6021e36f1c2c6a29eceffddf14d169d06166490c
AliReZa <alirezanet@outlook.com>
2021-07-15 03:31:37 +0430

    Add GetFilteringExpression and GetOrderingExpression to GridifyMapper

--- src/Core/GridifyQuery.cs
+++ src/Core/GridifyQuery.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Gridify.Syntax;
+
 namespace Gridify
 {
-   public class GridifyQuery : IGridifyQuery
+   public partial class GridifyQuery : IGridifyQuery
    {
       public GridifyQuery()
       {
@@ -20,5 +25,31 @@ namespace Gridify
       public short Page { get; set; }
       public int PageSize { get; set; }
       public string Filter { get; set; }
+
+      public Expression<Func<T, bool>> GetFilteringExpression<T>(IGridifyMapper<T> mapper = null)
+      {
+         if (string.IsNullOrWhiteSpace(Filter))
+            throw new GridifyQueryException("Filter is not defined");
+
+         mapper = mapper.FixMapper();
+
+         var syntaxTree = SyntaxTree.Parse(Filter);
+
+         if (syntaxTree.Diagnostics.Any())
+            throw new GridifyFilteringException(syntaxTree.Diagnostics.Last()!);
+
+         var queryExpression = ExpressionToQueryConvertor.GenerateQuery(syntaxTree.Root, this, mapper);
+         if (queryExpression == null) throw new GridifyQueryException("Can not create expression with current data");
+         return queryExpression;
+      }
+      public Expression<Func<T, object>> GetOrderingExpression<T>(IGridifyMapper<T> mapper = null)
+      {
+         mapper = mapper.FixMapper();
+         if (string.IsNullOrWhiteSpace(SortBy) || !mapper.HasMap(SortBy))
+            throw new GridifyQueryException("SortBy is not defined or not Found");
+         var expression = mapper.GetExpression(SortBy);
+         return expression;
+      }
+      
    }
 }
