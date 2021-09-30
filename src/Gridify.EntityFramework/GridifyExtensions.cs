111e63d855f8ef455d70afd3371eb580821394f7
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-09-30 18:10:41 +0330

    Add CancelationToken Support

--- src/Gridify.EntityFramework/GridifyExtensions.cs
+++ src/Gridify.EntityFramework/GridifyExtensions.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,6 @@ namespace Gridify.EntityFramework
 {
    public static partial class GridifyExtensions
    {
-      
       #region "EntityFramework Integration"
 
       public async static Task<QueryablePaging<T>> GridifyQueryableAsync<T>(this IQueryable<T> query, IGridifyQuery gridifyQuery,
@@ -24,6 +24,23 @@ namespace Gridify.EntityFramework
          mapper = mapper.FixMapper();
          var res = await query.GridifyQueryableAsync(gridifyQuery, mapper);
          return new Paging<T>(res.Count, await res.Query.ToListAsync());
+      }
+
+      public async static Task<QueryablePaging<T>> GridifyQueryableAsync<T>(this IQueryable<T> query, IGridifyQuery gridifyQuery,
+         IGridifyMapper<T> mapper, CancellationToken token)
+      {
+         query = query.ApplyFiltering(gridifyQuery, mapper);
+         var count = await query.CountAsync(token);
+         query = query.ApplyOrdering(gridifyQuery, mapper);
+         query = query.ApplyPaging(gridifyQuery);
+         return new QueryablePaging<T>(count, query);
+      }
+
+      public static async Task<Paging<T>> GridifyAsync<T>(this IQueryable<T> query, IGridifyQuery gridifyQuery, CancellationToken token, IGridifyMapper<T> mapper = null)
+      {
+         mapper = mapper.FixMapper();
+         var res = await query.GridifyQueryableAsync(gridifyQuery, mapper, token);
+         return new Paging<T>(res.Count, await res.Query.ToListAsync(token));
       }
 
       #endregion
