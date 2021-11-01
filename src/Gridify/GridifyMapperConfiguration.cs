0e133c474b53cd000defab95e1581dcbab5aeed4
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-11-01 15:44:35 +0330

    add some summary

--- src/Gridify/GridifyMapperConfiguration.cs
+++ src/Gridify/GridifyMapperConfiguration.cs
@@ -3,8 +3,16 @@ namespace Gridify
    public record GridifyMapperConfiguration
    {
       public bool CaseSensitive { get; set; }
-      public bool AllowNullSearch { get; set; } = true;
       
+      /// <summary>
+      /// This option enables the 'null' keyword in filtering operations
+      /// </summary>
+      public bool AllowNullSearch { get; set; } = true;
+      /// <summary>
+      /// If true, in filtering and ordering operations,
+      /// gridify doesn't return any exceptions when a mapping
+      /// is not defined for the fields
+      /// </summary>
       public bool IgnoreNotMappedFields { get; set; }
    }
 }
