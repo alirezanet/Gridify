ca1092aabdd7823f80c7858d49e4a58725001239
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-12-10 20:46:50 +0330

    change help message

--- docs/.vuepress/configs/themeConfig.ts
+++ docs/.vuepress/configs/themeConfig.ts
@@ -5,7 +5,7 @@ export const themeConfig: DefaultThemeOptions = {
    logo: '/gridify-light-logo.svg',
    logoDark: '/gridify-dark-logo.svg',
    editLinks: true,
-   // editLinkText: 'Help us improve this page!',
+   editLinkText: 'Help us improve this page!',
    contributors: false,
    docsRepo: 'alirezanet/gridify',
    docsBranch: 'master',
