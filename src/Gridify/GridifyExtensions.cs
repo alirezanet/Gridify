c4516ff20f509ff2198af2b154b59f9ad1af13e1
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-12-10 20:10:39 +0330

    Add new overload for ApplyPaging that accept page and pageSize

--- src/Gridify/GridifyExtensions.cs
+++ src/Gridify/GridifyExtensions.cs
@@ -304,6 +304,10 @@ namespace Gridify
          return query;
       }
 
+      public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, int page, int pageSize)
+      {
+         return query.Skip((page - 1) * pageSize).Take(pageSize);
+      }
       public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, IGridifyPagination? gridifyPagination)
       {
          if (gridifyPagination == null) return query;
