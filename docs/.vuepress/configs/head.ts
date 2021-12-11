37039256beb2004e1bc8910720788196d1156767
AliReZa Sabouri <7004080+alirezanet@users.noreply.github.com>
2021-12-11 22:22:19 +0330

    add social media  image

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -1,6 +1,14 @@
 import type { HeadConfig } from 'vuepress-vite';
 
 export const head: HeadConfig[] = [
+   // favicon
    ['link', { rel: 'icon', href: 'favicon.ico', type: "image/x-icon" }],
-   ['link', { rel: 'shortcut icon', href: 'favicon.ico', type: "image/x-icon" }]
+   ['link', { rel: 'shortcut icon', href: 'favicon.ico', type: "image/x-icon" }],
+
+   // social media image
+   ['meta', { property: 'og:image', content: 'social-logo.png' }],
+   ['meta', { property: 'og:image:type', content: 'image/png' }],
+   ['meta', { property: 'og:image:width', content: '1280' }],
+   ['meta', { property: 'og:image:height', content: '640' }],
+   ['meta', { property: 'og:image:title', content: 'Gridify' }],
 ]
