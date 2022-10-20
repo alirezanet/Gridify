819faeb0ef5205dbec38e962ff8b22ad10e16e15
AliReZa Sabouri <alirezanet@outlook.com>
2022-10-20 17:11:23 +0330

    refactor: compile is match regex

--- src/Gridify/GMap.cs
+++ src/Gridify/GMap.cs
@@ -17,7 +17,7 @@ public class GMap<T> : IGMap<T>
       Convertor = convertor;
    }
 
-   internal bool IsNestedCollection() => Regex.IsMatch(To.ToString(), @"\.Select\s*\(");
+   internal bool IsNestedCollection() => Regex.IsMatch(To.ToString(), @"\.Select\s*\(", RegexOptions.Compiled);
 
    public GMap(string from, Expression<Func<T, int, object?>> to, Func<string, object>? convertor = null)
    {
