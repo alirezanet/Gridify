f6653df2f34949e0f5e957c302240997f0baa18e
AliReza <alirezanet@outlook.com>
2023-09-21 23:26:42 +0200

    test: add test related to issue #120

---
+++ test/Gridify.Tests/Issue120Tests.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xunit;
+
+namespace Gridify.Tests;
+
+public class Issue120Tests
+{
+   [Fact]
+   public void ApplyFiltering_WhenPassedADoubleValue_ShouldWorkCorrectly()
+   {
+      CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en");
+      var query = new List<Test>()
+      {
+         new() { PropertyDouble = 0.2 },
+         new() { PropertyDouble = 0.6 },
+         new() { PropertyDouble = 0 },
+      }.AsQueryable();
+
+      var result = query.ApplyFiltering("propertyDouble > 0.5").ToList();
+
+      Assert.Single(result);
+   }
+
+   private class Test
+   {
+      public double PropertyDouble { get; set; }
+   }
+}
