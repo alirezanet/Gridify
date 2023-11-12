15e60ebb73a9017e92027a3e43f3513e6f28af00
AliReZa Sabouri <alirezanet@outlook.com>
2023-11-12 19:11:30 +0100

    refactor: add IFiltering IOrdering and IPagination interfaces

--- client/src/IGridifyQuery.ts
+++ client/src/IGridifyQuery.ts
@@ -1,6 +1,17 @@
-export interface IGridifyQuery {
-   page?: number;
-   pageSize?: number;
-   orderBy?: string;
+export interface IGridifyQuery
+   extends IGridifyPagination,
+      IGridifyFiltering,
+      IGridifyOrdering {}
+
+export interface IGridifyFiltering {
    filter?: string;
+}
+
+export interface IGridifyOrdering {
+   orderBy?: string;
+}
+
+export interface IGridifyPagination {
+   page: number;
+   pageSize: number;
 }
