6601bb4fdc4b95de4e8dbd700cc4b5f5109c4a9c
AliReZa Sabouri <alirezanet@outlook.com>
2024-06-21 17:59:54 +0200

    chore: add native linq comparision benchmark

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -7,7 +7,8 @@ public class Program
 {
    private static void Main()
    {
-      BenchmarkRunner.Run<LibraryComparisionFilteringBenchmark>();
+      // BenchmarkRunner.Run<LibraryComparisionFilteringBenchmark>();
+      BenchmarkRunner.Run<NativeLinqComparisionBenchmark>();
       // BenchmarkRunner.Run<LibraryComparisionFilteringBenchmark2>();
       // BenchmarkRunner.Run<GridifyMapperUsages>();
       // BenchmarkRunner.Run<QueryBuilderBuildBenchmark>();
