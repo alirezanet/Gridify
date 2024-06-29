fdb33a0539e34f9c83d8775385cb8737aa5a8903
AliReZa Sabouri <alirezanet@outlook.com>
2024-06-29 16:35:55 +0200

    refactor: test and improve DI extension method

--- src/Gridify/DependencyInjectionExtensions.cs
+++ src/Gridify/DependencyInjectionExtensions.cs
@@ -10,31 +10,28 @@ namespace Gridify;
 public static class DependencyInjectionExtensions
 {
    /// <summary>
-   /// Automatically scans an assembly for classes that inherit from GridifyMapper<T> and registers them in the Dependency Injection container.
+   /// Automatically scans an assembly for classes that inherit from GridifyMapper&lt;T&gt; and registers them in the Dependency Injection container.
    /// </summary>
    /// <param name="services">The IServiceCollection to which the mappers should be added.</param>
    /// <param name="assembly">The assembly to scan for GridifyMapper implementations.</param>
    /// <param name="lifetime">The service lifetime for the registered mappers (default is Singleton).</param>
    public static IServiceCollection AddGridifyMappers(this IServiceCollection services, Assembly assembly,
-      ServiceLifetime lifetime = ServiceLifetime.Singleton)
+       ServiceLifetime lifetime = ServiceLifetime.Singleton)
    {
-      var mapperType = typeof(GridifyMapper<>);
-
-      var mapperTypes = assembly.GetTypes()
+      var targetTypes = assembly.GetTypes()
          .Where(type =>
-            type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true } &&
-            type.BaseType.GetGenericTypeDefinition() == mapperType);
+            type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true, BaseType.Namespace: nameof(Gridify) } &&
+            type.BaseType.GetGenericTypeDefinition() == typeof(GridifyMapper<>));
 
-      foreach (var mapper in mapperTypes)
+      foreach (var type in targetTypes)
       {
-         var genericArguments = mapper.BaseType?.GetGenericArguments();
-         if (genericArguments is { Length: 1 })
-         {
-            var targetType = genericArguments[0];
-var interfaceType = typeof(IGridifyMapper<>).MakeGenericType(targetType);
-services.Add(new ServiceDescriptor(interfaceType, mapper, lifetime));
-         }
+         var genericArguments = type.BaseType?.GetGenericArguments();
+         if (genericArguments is not { Length: 1 }) continue;
+         var targetType = genericArguments[0];
+         var interfaceType = typeof(IGridifyMapper<>).MakeGenericType(targetType);
+         services.Add(new ServiceDescriptor(interfaceType, type, lifetime));
       }
+
       return services;
    }
 }
