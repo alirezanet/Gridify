e4754b129b6b7489822723cd45707ab66c5f0ad6
AliReZa Sabouri <alirezanet@outlook.com>
2024-06-29 20:43:12 +0200

    refactor: remove unnecessary changes

--- src/Gridify/DependencyInjectionExtensions.cs
+++ src/Gridify/DependencyInjectionExtensions.cs
@@ -11,7 +11,6 @@ namespace Gridify;
 public static class DependencyInjectionExtensions
 {
    private static readonly (Type BaseType, Type Interface) GridifyMapperType = (typeof(GridifyMapper<>), typeof(IGridifyMapper<>));
-   private static readonly (Type BaseType, Type Interface) QueryBuilderType = (typeof(QueryBuilder<>), typeof(IQueryBuilder<>));
 
    /// <summary>
    /// Automatically scans an assembly for classes that inherit from GridifyMapper&lt;T&gt; and registers them in the Dependency Injection container.
@@ -22,65 +21,17 @@ public static class DependencyInjectionExtensions
    public static IServiceCollection AddGridifyMappers(this IServiceCollection services, Assembly assembly,
        ServiceLifetime lifetime = ServiceLifetime.Singleton)
    {
-      return ScanAndAddByType(services, assembly, lifetime, GridifyMapperType);
-   }
-
-   /// <summary>
-   /// Automatically scans an assembly for classes that inherit from QueryBuilder&lt;T&gt; and registers them in the Dependency Injection container.
-   /// </summary>
-   /// <param name="services">The IServiceCollection to which the query builders should be added.</param>
-   /// <param name="assembly">The assembly to scan for QueryBuilder implementations.</param>
-   /// <param name="lifetime">The service lifetime for the registered query builders (default is Singleton).</param>
-   public static IServiceCollection AddGridifyQueryBuilders(this IServiceCollection services, Assembly assembly,
-      ServiceLifetime lifetime = ServiceLifetime.Singleton)
-   {
-      return ScanAndAddByType(services, assembly, lifetime, QueryBuilderType);
-   }
-
-   /// <summary>
-   /// Automatically scans an assembly for classes that inherit from either GridifyMapper&lt;T&gt; or QueryBuilder&lt;T&gt;
-   /// and registers them in the Dependency Injection container.
-   /// </summary>
-   /// <param name="services">The IServiceCollection to which the mappers and query builders should be added.</param>
-   /// <param name="assembly">The assembly to scan for GridifyMapper and QueryBuilder implementations.</param>
-   /// <param name="lifetime">The service lifetime for the registered types (default is Singleton).</param>
-   public static IServiceCollection AddGridify(this IServiceCollection services, Assembly assembly,
-      ServiceLifetime lifetime = ServiceLifetime.Singleton)
-   {
-      var types = assembly.GetTypes()
-         .Where(type => type is
-         { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true, BaseType.Namespace: nameof(Gridify) })
-         .Select(type => (type, typeDefinition: type.BaseType?.GetGenericTypeDefinition()))
-         .Where(q => q.typeDefinition != null && q.typeDefinition == QueryBuilderType.BaseType || q.typeDefinition == GridifyMapperType.BaseType);
-
-      foreach (var (type, typeDefinition) in types)
-      {
-         var genericArguments = type.BaseType?.GetGenericArguments();
-         if (genericArguments is not { Length: 1 }) continue;
-         var targetType = genericArguments[0];
-         var interfaceType = typeDefinition == GridifyMapperType.BaseType
-            ? GridifyMapperType.Interface.MakeGenericType(targetType)
-            : QueryBuilderType.Interface.MakeGenericType(targetType);
-         services.Add(new ServiceDescriptor(interfaceType, type, lifetime));
-      }
-
-      return services;
-   }
-
-   private static IServiceCollection ScanAndAddByType(IServiceCollection services, Assembly assembly, ServiceLifetime lifetime,
-      (Type BaseType, Type Interface) scanType)
-   {
       var targetTypes = assembly.GetTypes()
          .Where(type =>
             type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true, BaseType.Namespace: nameof(Gridify) } &&
-            type.BaseType.GetGenericTypeDefinition() == scanType.BaseType);
+            type.BaseType.GetGenericTypeDefinition() == GridifyMapperType.BaseType);
 
       foreach (var type in targetTypes)
       {
          var genericArguments = type.BaseType?.GetGenericArguments();
          if (genericArguments is not { Length: 1 }) continue;
          var targetType = genericArguments[0];
-         var interfaceType = scanType.Interface.MakeGenericType(targetType);
+         var interfaceType = GridifyMapperType.Interface.MakeGenericType(targetType);
          services.Add(new ServiceDescriptor(interfaceType, type, lifetime));
       }
 
--- test/Gridify.Tests/DependencyInjectionTests.cs
+++ test/Gridify.Tests/DependencyInjectionTests.cs
@@ -27,44 +27,6 @@ public class DependencyInjectionTests
       services.Count.Should().Be(2);
    }
 
-   [Fact]
-   public void AddGridifyQueryBuilders_ShouldRegisterQueryBuilders()
-   {
-      // Arrange
-      var services = new ServiceCollection();
-      var assemblyMock = GetAssembly();
-
-      // Act
-      services.AddGridifyQueryBuilders(assemblyMock);
-
-      // Assert
-      var serviceProvider = services.BuildServiceProvider();
-      serviceProvider.GetService<IQueryBuilder<TestEntity>>().Should().NotBeNull();
-      serviceProvider.GetService<IQueryBuilder<TestEntity2>>().Should().NotBeNull();
-      serviceProvider.GetService<TestQueryBuilder>().Should().BeNull();
-      services.Count.Should().Be(2);
-   }
-
-   [Fact]
-   public void AddGridify_ShouldRegisterBothMappersAndQueryBuilders()
-   {
-      // Arrange
-      var services = new ServiceCollection();
-      var assemblyMock = GetAssembly();
-
-      // Act
-      services.AddGridify(assemblyMock);
-
-      // Assert
-      var serviceProvider = services.BuildServiceProvider();
-      serviceProvider.GetService<IGridifyMapper<TestEntity>>().Should().NotBeNull();
-      serviceProvider.GetService<IGridifyMapper<TestEntity2>>().Should().NotBeNull();
-      serviceProvider.GetService<TestMapper>().Should().BeNull();
-      serviceProvider.GetService<IQueryBuilder<TestEntity>>().Should().NotBeNull();
-      serviceProvider.GetService<IQueryBuilder<TestEntity2>>().Should().NotBeNull();
-      serviceProvider.GetService<TestQueryBuilder>().Should().BeNull();
-      services.Count.Should().Be(4);
-   }
    private static Assembly GetAssembly()
    {
       var assemblyMock = Substitute.For<Assembly>();
@@ -72,8 +34,6 @@ public class DependencyInjectionTests
          typeof(TestMapper),
          typeof(AnotherTestMapper),
          typeof(NonGridifyMapper),
-         typeof(TestQueryBuilder),
-         typeof(AnotherTestQueryBuilder)
       ]);
       return assemblyMock;
    }
@@ -85,9 +45,6 @@ public class DependencyInjectionTests
 
    public class AnotherTestMapper : GridifyMapper<TestEntity2>;
 
-   public class TestQueryBuilder : QueryBuilder<TestEntity>;
-
-   public class AnotherTestQueryBuilder : QueryBuilder<TestEntity2>;
 
    // Non-Gridify types for testing
    public class NonGridifyMapper;
