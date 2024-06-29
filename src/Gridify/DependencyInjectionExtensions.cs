b79384541813892a52ed2a328ab553d5c9faee0d
AliReZa Sabouri <alirezanet@outlook.com>
2024-06-29 16:35:55 +0200

    feat: add automatic QueryBuilder registration to DI

--- src/Gridify/DependencyInjectionExtensions.cs
+++ src/Gridify/DependencyInjectionExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,32 +10,85 @@ namespace Gridify;
 /// </summary>
 public static class DependencyInjectionExtensions
 {
+   private static readonly Type GridifyMapperType = typeof(GridifyMapper<>);
+   private static readonly Type QueryBuilderType = typeof(QueryBuilder<>);
+
    /// <summary>
-   /// Automatically scans an assembly for classes that inherit from GridifyMapper<T> and registers them in the Dependency Injection container.
+   /// Automatically scans an assembly for classes that inherit from GridifyMapper&lt;T&gt; and registers them in the Dependency Injection container.
    /// </summary>
    /// <param name="services">The IServiceCollection to which the mappers should be added.</param>
    /// <param name="assembly">The assembly to scan for GridifyMapper implementations.</param>
    /// <param name="lifetime">The service lifetime for the registered mappers (default is Singleton).</param>
    public static IServiceCollection AddGridifyMappers(this IServiceCollection services, Assembly assembly,
-      ServiceLifetime lifetime = ServiceLifetime.Singleton)
+       ServiceLifetime lifetime = ServiceLifetime.Singleton)
    {
-      var mapperType = typeof(GridifyMapper<>);
-
       var mapperTypes = assembly.GetTypes()
-         .Where(type =>
-            type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true } &&
-            type.BaseType.GetGenericTypeDefinition() == mapperType);
+          .Where(type =>
+              type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true } &&
+              type.BaseType.GetGenericTypeDefinition() == GridifyMapperType);
 
       foreach (var mapper in mapperTypes)
       {
          var genericArguments = mapper.BaseType?.GetGenericArguments();
-         if (genericArguments is { Length: 1 })
-         {
-            var targetType = genericArguments[0];
-var interfaceType = typeof(IGridifyMapper<>).MakeGenericType(targetType);
-services.Add(new ServiceDescriptor(interfaceType, mapper, lifetime));
-         }
+         if (genericArguments is not { Length: 1 }) continue;
+         var targetType = genericArguments[0];
+         var interfaceType = GridifyMapperType.MakeGenericType(targetType);
+         services.Add(new ServiceDescriptor(interfaceType, mapper, lifetime));
       }
+
+      return services;
+   }
+
+   /// <summary>
+   /// Automatically scans an assembly for classes that inherit from QueryBuilder&lt;T&gt; and registers them in the Dependency Injection container.
+   /// </summary>
+   /// <param name="services">The IServiceCollection to which the query builders should be added.</param>
+   /// <param name="assembly">The assembly to scan for QueryBuilder implementations.</param>
+   /// <param name="lifetime">The service lifetime for the registered query builders (default is Singleton).</param>
+   public static IServiceCollection AddGridifyQueryBuilders(this IServiceCollection services, Assembly assembly,
+      ServiceLifetime lifetime = ServiceLifetime.Singleton)
+   {
+      var queryBuilderTypes = assembly.GetTypes()
+         .Where(type =>
+            type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true } &&
+            type.BaseType.GetGenericTypeDefinition() == QueryBuilderType);
+
+      foreach (var queryBuilder in queryBuilderTypes)
+      {
+         var genericArguments = queryBuilder.BaseType?.GetGenericArguments();
+         if (genericArguments is not { Length: 1 }) continue;
+         var targetType = genericArguments[0];
+         var interfaceType = QueryBuilderType.MakeGenericType(targetType);
+         services.Add(new ServiceDescriptor(interfaceType, queryBuilder, lifetime));
+      }
+
+      return services;
+   }
+
+   /// <summary>
+   /// Automatically scans an assembly for classes that inherit from either GridifyMapper&lt;T&gt; or QueryBuilder&lt;T&gt;
+   /// and registers them in the Dependency Injection container.
+   /// </summary>
+   /// <param name="services">The IServiceCollection to which the mappers and query builders should be added.</param>
+   /// <param name="assembly">The assembly to scan for GridifyMapper and QueryBuilder implementations.</param>
+   /// <param name="lifetime">The service lifetime for the registered types (default is Singleton).</param>
+   public static IServiceCollection AddGridify(this IServiceCollection services, Assembly assembly,
+      ServiceLifetime lifetime = ServiceLifetime.Singleton)
+   {
+      var types = assembly.GetTypes()
+         .Where(type =>
+            type is { IsAbstract: false, IsGenericTypeDefinition: false, BaseType.IsGenericType: true } &&
+            (type.BaseType.GetGenericTypeDefinition() == QueryBuilderType || type.BaseType.GetGenericTypeDefinition() == GridifyMapperType));
+
+      foreach (var type in types)
+      {
+         var genericArguments = type.BaseType?.GetGenericArguments();
+         if (genericArguments is not { Length: 1 }) continue;
+         var targetType = genericArguments[0];
+         var interfaceType = type.MakeGenericType(targetType);
+         services.Add(new ServiceDescriptor(interfaceType, interfaceType, lifetime));
+      }
+
       return services;
    }
 }
