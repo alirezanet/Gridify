a7e9e8ab68690d3b050ad48183457ac4c22ba722
AliReZa Sabouri <alirezanet@outlook.com>
2024-08-16 14:18:45 +0200

    test: add string operator test cases

--- test/Gridify.Tests/IssueTests/Issue191Tests.cs
+++ test/Gridify.Tests/IssueTests/Issue191Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -9,13 +10,13 @@ public class Issue191Tests
    private readonly List<TestClass> _fakeRepository = [.. GridifyExtensionsShould.GetSampleData()];
 
    [Fact]
-   public void ApplyFiltering_GlobalCaseInsensitiveSearch()
+   public void ApplyFiltering_Equals_GlobalCaseInsensitiveSearch()
    {
       var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
 
       var gq = new GridifyQuery { Filter = "name=BOB" };
 
-      var expected = _fakeRepository.Where(q => q.Name!.ToLower() == "BOB".ToLower()).ToList();
+      var expected = _fakeRepository.Where(q => string.Equals(q.Name!, "BOB", StringComparison.InvariantCultureIgnoreCase)).ToList();
 
       var actual = _fakeRepository.AsQueryable()
           .ApplyFiltering(gq, mapper)
@@ -25,4 +26,131 @@ public class Issue191Tests
       Assert.Equal(expected, actual);
       Assert.True(actual.Any());
    }
+
+   [Fact]
+   public void ApplyFiltering_NotEquals_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name!=BOB" };
+
+      var expected = _fakeRepository.Where(q => !string.Equals(q.Name!, "BOB", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_Contains_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name=*BO" };
+
+      var expected = _fakeRepository.Where(q => q.Name!.Contains("BO", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_NotContains_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name!*BO" };
+
+      var expected = _fakeRepository.Where(q => !q.Name!.Contains("BO", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_StartWith_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name^BO" };
+
+      var expected = _fakeRepository.Where(q => q.Name!.StartsWith("BO", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_NotStartWith_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name!^BO" };
+
+      var expected = _fakeRepository.Where(q => !q.Name!.StartsWith("BO", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_EndWith_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name$OB" };
+
+      var expected = _fakeRepository.Where(q => q.Name!.EndsWith("OB", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
+   [Fact]
+   public void ApplyFiltering_NotEndWith_GlobalCaseInsensitiveSearch()
+   {
+      var mapper = new GridifyMapper<TestClass>(m => m.CaseInsensitiveFiltering = true).GenerateMappings();
+
+      var gq = new GridifyQuery { Filter = "name!$OB" };
+
+      var expected = _fakeRepository.Where(q => !q.Name!.EndsWith("OB", StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+      var actual = _fakeRepository.AsQueryable()
+         .ApplyFiltering(gq, mapper)
+         .ToList();
+
+      Assert.Equal(expected.Count, actual.Count);
+      Assert.Equal(expected, actual);
+      Assert.True(actual.Any());
+   }
+
 }
