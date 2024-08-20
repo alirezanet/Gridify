7092bd16c0967522f8e7acd0e791946de8054c54
mohit <kukreja.mohit@gmail.com>
2024-08-20 14:33:09 -0400

    fix: issue 204

---
+++ test/Gridify.Tests/IssueTests/Issue204Tests.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Gridify.Tests.IssueTests;
+
+public class Issue204Tests
+{
+    [Fact]
+    public void ApplyFiltering_NotEquals_ShouldMatch_NullItems()
+    {
+        // arrange
+        var dataSource = new List<Test>()
+        {
+            new() {FavouriteColorList = ["Green", "Blue"]},
+            new() {FavouriteColorList = ["White", "Yellow"]},
+            new() { FavouriteColorList = null },
+        }.AsQueryable();
+      
+        var expected = dataSource.Where(q => q.FavouriteColorList == null || !q.FavouriteColorList.Contains("Green")).ToList();
+        var actual = dataSource.ApplyFiltering("FavouriteColorList!=Green").ToList();
+
+        // assert
+        Assert.Equal(expected.Count, actual.Count);
+        Assert.Equal(expected, actual);
+        Assert.True(actual.Any());
+    }
+
+    private class Test
+    {
+        public List<string> FavouriteColorList { get; set; }
+    }
+}
