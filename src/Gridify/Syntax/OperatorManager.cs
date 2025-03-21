f4aab50012c150cbca6d5921fc4c90b85f61d5d4
Glacor Soul <glacorsoul@gmail.com>
2025-03-21 08:22:57 +0000

    chore: #262 simplify operator registering and removal

--- src/Gridify/Syntax/OperatorManager.cs
+++ src/Gridify/Syntax/OperatorManager.cs
@@ -9,7 +9,7 @@ namespace Gridify.Syntax;
 public class OperatorManager
 {
    private readonly ConcurrentDictionary<string, IGridifyOperator> _operators = new();
-   public IEnumerable<IGridifyOperator> Operators => _operators?.Values ?? Enumerable.Empty<IGridifyOperator>();
+   public IEnumerable<IGridifyOperator> Operators => _operators.Values;
 
    public void Register<TOperator>() where TOperator : IGridifyOperator
    {
@@ -20,7 +20,8 @@ public class OperatorManager
    public void Register(IGridifyOperator gridifyOperator)
    {
       Validate(gridifyOperator.GetOperator());
-      _operators.Add(gridifyOperator);
+      var @operator = gridifyOperator.GetOperator();
+      _operators.TryAdd(@operator, gridifyOperator);
    }
 
    public void Register(string @operator, Expression<OperatorParameter> handler)
@@ -28,7 +29,7 @@ public class OperatorManager
       Validate(@operator);
 
       var customOperator = new GridifyOperator(@operator, handler);
-      _operators.Add(customOperator);
+      _operators.TryAdd(@operator, customOperator);
    }
    public void Remove<TOperator>() where TOperator : IGridifyOperator
    {
@@ -36,28 +37,11 @@ public class OperatorManager
       Remove(gridifyOperator.GetOperator());
    }
 
-   public void Remove(string @operator) => _operators?.Remove(@operator);
+   public void Remove(string @operator) => _operators.TryRemove(@operator, out _);
 
    private static void Validate(string @operator)
    {
       if (!@operator.StartsWith("#"))
          throw new ArgumentException("Custom operator must start with '#'");
-   }
-}
-
-/// <summary>
-/// Retry if for some reason we couldn't add/remove the operator
-/// </summary>
-internal static class OperatorManagerExtensions
-{
-   internal static void Add(this ConcurrentDictionary<string, IGridifyOperator> operators, IGridifyOperator gridifyOperator)
-   {
-      var @operator = gridifyOperator.GetOperator();
-      operators.TryAdd(@operator, gridifyOperator);
-   }
-
-   internal static void Remove(this ConcurrentDictionary<string, IGridifyOperator> operators, string @operator)
-   {
-      operators.TryRemove(@operator, out _);
    }
 }
