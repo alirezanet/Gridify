dccb5de5f42ebb245d10630448a21919f02d4633
AliReZa Sabouri <alirezanet@outlook.com>
2026-03-08 00:10:01 +0100

    fix: replace rollup-plugin-terser with @rollup/plugin-terser for rollup v4 compatibility

--- client/rollup.config.mjs
+++ client/rollup.config.mjs
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default [
   // ESM bundle
